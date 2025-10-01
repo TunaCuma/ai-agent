41feefaa86836904f6c138b68ca757683c4f375e
TunaCuma <tuna.cuma@udemy.com>
2025-10-01 15:56:17 +0300

    add tests and fixed quirks

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,35 +1,24 @@
 import os
 
+def get_files_info(working_directory: str, directory="."):
+    """A tool call function for an AI agent to use"""
+    try:
+        full_path = os.path.abspath(os.path.join(working_directory, directory))
 
-def get_files_info(working_directory, directory="."):
-    full_path = os.path.join(working_directory, directory)
+        if not os.path.isdir(full_path):
+            return f'Error: "{directory}" is not a directory'
+        if not full_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(full_path):
-        raise Exception(f'Error: "{directory}" is not a directory')
-    if not full_path.startswith(working_directory):
-        raise Exception(
-            f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        file_infos_string = "\n".join(
+            [
+                f"- {file}: file_size={os.path.getsize(os.path.join(full_path, file))} bytes, is_dir={os.path.isdir(os.path.join(full_path, file))}"
+                for file in os.listdir(full_path)
+            ]
         )
-
-    file_infos = [
-        {
-            "file": file,
-            "file_size": os.path.getsize(os.path.join(full_path, file)),
-            "is_dir": os.path.isdir(os.path.join(full_path, file)),
-        }
-        for file in os.listdir(full_path)
-    ]
-
-    file_infos_string = "\n".join(
-        [
-            f"- {file_info['file']}: file_size={file_info['file_size']} bytes, is_dir={file_info['is_dir']}"
-            for file_info in file_infos
-        ]
-    )
+    except OSError as e:
+        return f"Error: An OSError has occured: {e}"
+    except Exception as e:
+        return f"Error: {e}"
 
     return file_infos_string
-
-
-print(get_files_info(os.path.abspath(""), "functions/"))
-
-# print(os.path.abspath("aa"))
---
+++ tests.py
@@ -0,0 +1,15 @@
+from functions.get_files_info import get_files_info
+
+
+def main():
+    print(get_files_info("calculator", "."))
+    print("===================")
+    print(get_files_info("calculator", "pkg"))
+    print("===================")
+    print(get_files_info("calculator", "/bin"))
+    print("===================")
+    print(get_files_info("calculator", "../"))
+    print("===================")
+
+if __name__ == "__main__":
+    main()
